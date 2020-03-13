85c61e313ddfcccbd5ae7e5aea3a642cc2ba1fa5
Spencer Purcell <spencerpurcell@Spencers-MacBook-Pro.local>
2020-03-12 17:48:40 -0700

    wrking i believe

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+const db = require("./models");
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,9 +18,17 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+db.Workout.create({name: "Bench Press"})
+  .then(dbWorkout => {
+    console.log(dbWorkout);
+  })
+  .catch(({message}) => {
+    console.log(message);
+  })
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
